e9294221482d744bf72163003e04bfb40691bae7
nfonjeannoel <nfonjeannoel1@gmail.com>
2022-01-05 15:54:19 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from playwright.sync_api import sync_playwright
 
+
+# def getProxy():
+#     return None
+#
+#
+# with sync_playwright() as p:
+#     browser = p.chromium.launch(headless=False, proxy={getProxy()})
+#     page = browser.new_page()
+#     page.goto("https://kamerjam.blogspot.com/")
+#     print(page.title())
+#     browser.close()
+
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto("https://kamerjam.blogspot.com/")
-    print(page.title())
+    browser = p.webkit.launch(headless=False, proxy={
+        "server": 'server-address:port',
+        'username': 'My_user',
+        "password": 'My_password',
+    })
+    context = browser.newContext()
+    page = context.newPage()
+    page.goto('https://whoer.net')
+    page.screenshot(path='whoer.png')
     browser.close()
